d04ac7fb50577bab4960eb2fba7aa08ab85e5156
sᴀᴍɪᴅᴀɪᴛᴇs <112374934+Samidaites2@users.noreply.github.com>
2023-05-14 01:38:30 +0700

    Update sangmata.py

--- EmikoRobot/modules/sangmata.py
+++ EmikoRobot/modules/sangmata.py
@@ -16,9 +16,9 @@ async def lastname(steal):
         await puki.edit("```Please Reply To User Message.```")
         return
     message = await steal.get_reply_message()
-    chat = "@SangMataInfo_bot"
+    chat = "@SangMata_beta_bot"
     user_id = message.sender.id
-    id = f"/search_id {user_id}"
+    id = f"{user_id}"
     if message.sender.bot:
         await puki.edit("```Reply To Real User's Message.```")
         return
@@ -31,7 +31,7 @@ async def lastname(steal):
                 response = await conv.get_response()
             except YouBlockedUserError:
                 await steal.reply(
-                    "```Error, report to @kenbotsupport```"
+                    "```Error, report to @cemarasupport```"
                 )
                 return
             if r.text.startswith("Name"):
