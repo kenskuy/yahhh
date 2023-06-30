db9502365d6e79d329e6bdaa03010b7cc924b03f
OTANCABUL <119916934+OTANCABUL@users.noreply.github.com>
2023-07-01 00:10:55 +0700

    __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -81,7 +81,6 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-PM_START_TEXT = """
 *Yoo mek {} !*
 ✪ Ini bot untuk manage ama musikan di grup lu pada [✨](https://telegra.ph/file/6eb04c66d9afd5babd604.jpg)
 ────────────────────────
